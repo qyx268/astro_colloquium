2d3231fa1bf860410c49b9ce30c759c1f5ee3d6d
qyx268 <Yuxiang.L.Qin@gmail.com>
2021-04-13 19:07:57 +1000

    first push

---
+++ publish_talk.py
@@ -0,0 +1,15 @@
+import sys, os
+
+ip_arr = {'date': ['date in yyyymmdd', ''], 
+'time': ['time as xxp.m.', '12 p.m.'], 
+'location':['location', 'David Caro building, Level 7 conference room'],
+'name':['speaker name as Dr. xx xx', ''],
+'affiliation':['speaker affiliation', ''],
+'title':['talk title', ''],
+'abstract':['talk abstract', ''],
+'special':['special note for talk - leave blank if nothing','-'],
+'position':['speaker position - postdoc/faculty/etc.',''],
+'photo':['speaker image name - must be in images. normally stored as flname.jpg', 'no_image.png'],
+'email':['speaker email as xx[at]xx.com', ''],
+'talkfile':['slide name','-']
+}
